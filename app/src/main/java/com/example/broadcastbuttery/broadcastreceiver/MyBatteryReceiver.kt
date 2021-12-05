7ffafbb8afb4bebba136df35165352dade5fa722
Rahaf Nasser <rahaf.nasser.ruh.k@tuwaiq.edu.sa>
2021-12-05 21:38:13 +0300

    update

--- app/src/main/java/com/example/broadcastbuttery/broadcastreceiver/MyBatteryReceiver.kt
+++ app/src/main/java/com/example/broadcastbuttery/broadcastreceiver/MyBatteryReceiver.kt
@@ -3,13 +3,19 @@ package com.example.broadcastbuttery.broadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.BatteryManager
 import android.widget.TextView
 
 class MyBatteryReceiver(val textview: TextView) : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         val batteryLevel = intent?.getIntExtra("level", 0)
-        textview.text =
-            if (batteryLevel <= 20) "Low" else if (batteryLevel > 20 && batteryLevel <= 80) "Full" else "Normal"
+        textview.text = if (intent.getIntExtra(
+                BatteryManager.EXTRA_STATUS,
+                -1
+            ) == BatteryManager.BATTERY_STATUS_CHARGING
+        )
+            "Charging"
+        else if (batteryLevel <= 20) "Low" else if (batteryLevel > 20 && batteryLevel <= 80) "Normal" else "Full"
     }
 }
